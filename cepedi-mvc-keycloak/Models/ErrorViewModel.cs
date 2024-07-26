cef1da8b172ce9652a5674772001bc7a54f22c77
luannfirme <luannfirme@gmail.com>
2024-07-26 15:04:10 -0300

    Adicionar arquivos de projeto.

---
+++ cepedi-mvc-keycloak/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cepedi_mvc_keycloak.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
