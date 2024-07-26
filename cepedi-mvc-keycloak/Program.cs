544016f4d7245d7e78a14cbdf098d8ad952e18f1
luannfirme <luannfirme@gmail.com>
2024-07-26 16:24:56 -0300

    keycloak funcionando

--- cepedi-mvc-keycloak/Program.cs
+++ cepedi-mvc-keycloak/Program.cs
@@ -1,4 +1,32 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+})
+    .AddCookie(opt =>
+    {
+        opt.ExpireTimeSpan = TimeSpan.FromMinutes(1);
+    })
+    .AddOpenIdConnect(options =>
+    {
+        options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.Authority = $"{builder.Configuration["Keycloak:auth-server-url"]}realms/{builder.Configuration["Keycloak:realm"]}";
+        options.ClientId = builder.Configuration["Keycloak:resource"];
+        options.ResponseType = OpenIdConnectResponseType.CodeIdTokenToken;
+        options.UsePkce = true;
+        options.Scope.Add("profile");
+        options.SaveTokens = true;
+        options.GetClaimsFromUserInfoEndpoint = true;
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters = new TokenValidationParameters() { RoleClaimType = "roles" };
+    });
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -18,6 +46,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
