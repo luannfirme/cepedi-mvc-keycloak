04e743e6481471d21da6c8f64720cfeb296e22ae
luannfirme <luannfirme@gmail.com>
2024-07-26 23:44:05 -0300

    configuração de keycloak por injeção de dependência

---
+++ cepedi-mvc-keycloak/Configurations/KeycloakConfig.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+
+namespace cepedi_mvc_keycloak.Configurations
+{
+    public static class KeycloakConfig
+    {
+        public static IServiceCollection ConfigurarKeycloak(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+            .AddCookie(opt =>
+            {
+                opt.ExpireTimeSpan = TimeSpan.FromMinutes(1);
+            })
+            .AddOpenIdConnect(options =>
+            {
+                options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.Authority = $"{configuration["Keycloak:auth-server-url"]}realms/{configuration["Keycloak:realm"]}";
+                options.ClientId = configuration["Keycloak:resource"];
+                options.ResponseType = OpenIdConnectResponseType.CodeIdTokenToken;
+                options.UsePkce = true;
+                options.Scope.Add("profile");
+                options.SaveTokens = true;
+                options.GetClaimsFromUserInfoEndpoint = true;
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new TokenValidationParameters() { RoleClaimType = "roles" };
+            });
+
+            return services;
+        }
+    }
+}
--- cepedi-mvc-keycloak/Program.cs
+++ cepedi-mvc-keycloak/Program.cs
@@ -1,43 +1,16 @@
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
-using Microsoft.IdentityModel.Protocols.OpenIdConnect;
-using Microsoft.IdentityModel.Tokens;
+using cepedi_mvc_keycloak.Configurations;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
-})
-    .AddCookie(opt =>
-    {
-        opt.ExpireTimeSpan = TimeSpan.FromMinutes(1);
-    })
-    .AddOpenIdConnect(options =>
-    {
-        options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-        options.Authority = $"{builder.Configuration["Keycloak:auth-server-url"]}realms/{builder.Configuration["Keycloak:realm"]}";
-        options.ClientId = builder.Configuration["Keycloak:resource"];
-        options.ResponseType = OpenIdConnectResponseType.CodeIdTokenToken;
-        options.UsePkce = true;
-        options.Scope.Add("profile");
-        options.SaveTokens = true;
-        options.GetClaimsFromUserInfoEndpoint = true;
-        options.RequireHttpsMetadata = false;
-        options.TokenValidationParameters = new TokenValidationParameters() { RoleClaimType = "roles" };
-    });
+builder.Services.ConfigurarKeycloak(builder.Configuration);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
